a08bcb8e2c9d26a789e2c0968a7caae3afc21579
Vito Cairone <vitocairone@gmail.com>
2017-05-14 17:50:02 -0700

    set sensible spell data

--- spells.js
+++ spells.js
@@ -1,33 +1,54 @@
 var SpellBook = {
-  'Pew': {
-    dtype: 'spell',
-    requires: 'any',
-    costFraction: 1 / 7,
-    damageX: 1.0
+  'Blast': {
+    costFraction: 3,
+    damageX: 5.5
   },
-  'Force Blast': {
-    dtype: 'spell',
-    requires: 'any',
-    costFraction: 1 / 3,
-    damageX: 1.0,
+
+  // EFAWILD
+  'Rock': {
+    cost: 30,
+    damage: 150
+    selfStatMods: { power: +25, aim: -25 }
+    selfStatModOn: 'cast'
+    selfStatModFor: 'spell'
   },
-  'Power Blast': {
-    dtype: 'spell',
-    requires: 'any',
-    costFraction: 2 / 3,
-    damageX: 1.0
+  'Ice': {
+    cost: 20,
+    damage: 110
+  }, 
+  'Fire': {
+    cost: 25,
+    damage: 135
   },
-  'Ultra Blast' {
-    dtype: 'spell',
-    requires: 'any',
-    costFraction: 1,
-    damageX: 1.0
+  'Bolt': {
+    cost: 40,
+    damage: 220
+  },
+  'Water': {
+    cost: 16,
+    damage: 80
+  },
+  'Cure': {
+    cost: 21,
+    heal: 100,
+    target: 'ally',
+    targetPriority: 'lowestHP'
+  },
+  'Drain': {
+    cost: 25,
+    damage: 50,
+    healSelfBy: 'damage'
+  },
+  
+  'Void': {
+    cost: 0,
+    heal: 0,
+    target: 'self'
   }
 }
 
-var SpellList = Object.keys(SpellBook);
-
 // set keys as .name on each objects as well
+var SpellList = Object.keys(SpellBook);
 for (var i = 0; i < SpellList.length; i++) {
   var spellName = SpellList[i];
   SpellBook[spellName].name = spellName;
